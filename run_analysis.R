530287173a1c42e8ceb79a2811722d5757e00756
Joe Schaal <joe@schaal.net>
2014-11-06 14:10:27 -0500

    Converted to a looped version

--- run_analysis.R
+++ run_analysis.R
@@ -2,12 +2,20 @@ library(data.table)
 library(dplyr)
 library(tidyr)
 
-#Load training data
+#   Initialize Variables
+dataSetNames <- c("train","test")
+subjectFiles <- c("./data/UCI HAR Dataset/train/subject_train.txt","./data/UCI HAR Dataset/test/subject_test.txt")
+activityFiles <- c("./data/UCI HAR Dataset/train/y_train.txt","./data/UCI HAR Dataset/test/y_test.txt")
+measurementFiles <- c("./data/UCI HAR Dataset/train/x_train.txt","./data/UCI HAR Dataset/test/x_test.txt")
+dfData <- data.frame()
+
+#Load Common Data
 print("Loading Common Data")
 
 fileName <- "./data/UCI HAR Dataset/features.txt"
 features <- read.table(fileName, col.names = c("field","feature"), stringsAsFactors = FALSE)
 fieldNames <- features[,2]
+rm(features)
 
 fileName <- "./data/UCI HAR Dataset/activity_labels.txt"
 rawActivityLabels <- read.table(fileName, col.names = c("ActivityClass","Activity"))
@@ -15,76 +23,58 @@ dtActivityLabels <- data.table(rawActivityLabels)
 rm(rawActivityLabels)
 setkey(dtActivityLabels,ActivityClass)
 
+#Load Measurements
+
 print("Loading Training Data")
+for (i in 1:length(dataSetNames)) {
+    
+    
+    fileName <- subjectFiles[i]
+    print(paste("Reading Subject File:",fileName))
+    rawTrainSubjects <- read.table(fileName,col.names="Subject")
+    
+    
+    fileName <- activityFiles[i]
+    print(paste("Reading Subject Activity File:",fileName))
+    rawTrainActivities  <- read.table(fileName,col.names = "ActivityClass")
+    dtTrainActivities <- data.table(rawTrainActivities)
+    rm(rawTrainActivities)
+    setkey(dtTrainActivities,ActivityClass)
+    
+    dtActivityDetail <- merge(dtTrainActivities,dtActivityLabels)
+    
+    
+    fileName <- measurementFiles[i]
+    print(paste("Reading Subject Measurement File:",fileName))
+    rawTrainData <- read.table(fileName)
+    print(paste("Records Read:",as.character(nrow(rawTrainData))))
+    
+    print("Transforming Data")
+    tblTrainData <- tbl_df(rawTrainData)
+    rm(rawTrainData)
+    
+    columnPatterns<- "mean\\(\\)|std\\(\\)"
+    subColumns  <- grep(columnPatterns,fieldNames)
+    subColumnNames  <- colnames(tblTrainData)[subColumns]
+    tblTrainSubSet  <- select(tblTrainData,one_of(subColumnNames))
+    rm(tblTrainData)
+    
+    columnPatterns<- "\\(\\)"
+    cleanNames <- gsub(columnPatterns,"",fieldNames)
+    cleanSubNames <- cleanNames[subColumns]
+    xyzDimensions <-  grep("X$|Y$|Z$",cleanSubNames)
+    cleanSubNames[-xyzDimensions] <- paste(cleanSubNames[-xyzDimensions],"-Other",sep="")
+    
+    colnames(tblTrainSubSet) <- cleanSubNames
+    tblTrainSubSet <- mutate(tblTrainSubSet, Activity = dtActivityDetail$Activity, Subject = rawTrainSubjects$Subject)
+    rm(rawTrainSubjects)
+    dfData <- rbind(dfData,tblTrainSubSet)
+    rm(tblTrainSubSet)
 
-fileName <- "./data/UCI HAR Dataset/train/subject_train.txt"
-rawTrainSubjects <- read.table(fileName,col.names="Subject")
+    
+}
 
-
-fileName <- "./data/UCI HAR Dataset/train/y_train.txt"
-rawTrainActivities  <- read.table(fileName,col.names = "ActivityClass")
-dtTrainActivities <- data.table(rawTrainActivities)
-rm(rawTrainActivities)
-setkey(dtTrainActivities,ActivityClass)
-
-dtActivityDetail <- merge(dtTrainActivities,dtActivityLabels)
-
-
-fileName <- "./data/UCI HAR Dataset/train/x_train.txt"
-rawTrainData <- read.table(fileName)
-
-tblTrainData <- tbl_df(rawTrainData)
-rm(rawTrainData)
-
-columnPatterns<- "mean\\(\\)|std\\(\\)"
-subColumns  <- grep(columnPatterns,fieldNames)
-subColumnNames  <- colnames(tblTrainData)[subColumns]
-tblTrainSubSet  <- select(tblTrainData,one_of(subColumnNames))
-rm(tblTrainData)
-
-columnPatterns<- "\\(\\)"
-cleanNames <- gsub(columnPatterns,"",fieldNames)
-cleanSubNames <- cleanNames[subColumns]
-xyzDimensions <-  grep("X$|Y$|Z$",cleanSubNames)
-cleanSubNames[-xyzDimensions] <- paste(cleanSubNames[-xyzDimensions],"-Other",sep="")
-
-colnames(tblTrainSubSet) <- cleanSubNames
-tblTrainSubSet <- mutate(tblTrainSubSet, Activity = dtActivityDetail$Activity, Subject = rawTrainSubjects$Subject)
-
-#Load test data
-
-print("Loading Test Data")
-
-fileName <- "./data/UCI HAR Dataset/test/subject_test.txt"
-rawTestSubjects <- read.table(fileName,col.names="Subject")
-
-fileName <- "./data/UCI HAR Dataset/test/y_test.txt"
-rawTestActivities  <- read.table(fileName,col.names = "ActivityClass")
-dtTestActivities <- data.table(rawTestActivities)
-rm(rawTestActivities)
-setkey(dtTestActivities,ActivityClass)
-
-dtActivityDetail <- merge(dtTestActivities,dtActivityLabels)
-
-
-fileName <- "./data/UCI HAR Dataset/test/x_test.txt"
-rawTestData <- read.table(fileName)
-
-tblTestData <- tbl_df(rawTestData)
-rm(rawTestData)
-
-tblTestSubSet  <- select(tblTestData,one_of(subColumnNames))
-rm(tblTestData)
-
-colnames(tblTestSubSet) <- cleanSubNames
-tblTestSubSet <- mutate(tblTestSubSet, Activity = dtActivityDetail$Activity, 
-                        Subject = rawTestSubjects$Subject)
-
-print("Merging UnTidy Data")
-
-dfData <-  merge(tblTrainSubSet,tblTestSubSet,all = TRUE)
-
-
+print(paste("Total Data Transformed:",as.character(nrow(dfData))))
 save(dfData, file = "data/MergedData.RData")
 
 print("Data Merging Complete")
@@ -104,3 +94,5 @@ tblData %>%
 write.table(x = tblTidy,"./data/TidyData.txt", row.name=FALSE)
 
 print("Data Tidying Complete")
+print(paste("Total Tidy Data Rows:",as.character(nrow(tblTidy))))
+
