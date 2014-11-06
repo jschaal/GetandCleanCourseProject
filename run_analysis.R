a3c4d7052b3e330f9a7caa12057f81c8c0fe69e5
Joe Schaal <joe@schaal.net>
2014-11-05 22:46:46 -0500

    Working merge version

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,7 @@
-
 library(data.table)
 library(dplyr)
 
+#Load training data
 fileName <- "./data/UCI HAR Dataset/features.txt"
 features <- read.table(fileName, col.names = c("field","feature"), stringsAsFactors = FALSE)
 fieldNames <- features[,2]
@@ -33,11 +33,46 @@ rm(rawTrainData)
 columnPatterns<- "mean\\(\\)|std\\(\\)"
 subColumns  <- grep(columnPatterns,fieldNames)
 subColumnNames  <- colnames(tblTrainData)[subColumns]
-tblSubSet  <- select(tblTrainData,one_of(subColumnNames))
+tblTrainSubSet  <- select(tblTrainData,one_of(subColumnNames))
 rm(tblTrainData)
+
 columnPatterns<- "\\(\\)"
 cleanNames <- gsub(columnPatterns,"",fieldNames)
-colnames(tblSubSet) <- cleanNames[subColumns]
-tblSubSet <- mutate(tblSubSet, Activity = dtActivityDetail$Activity, Subject = rawTrainSubjects$Subject)
+cleanSubNames <- cleanNames[subColumns]
+xyzDimensions <-  grep("X$|Y$|Z$",cleanSubNames)
+cleanSubNames[-xyzDimensions] <- paste(cleanSubNames[-xyzDimensions],"-Other",sep="")
+
+colnames(tblTrainSubSet) <- cleanSubNames
+tblTrainSubSet <- mutate(tblTrainSubSet, Activity = dtActivityDetail$Activity, Subject = rawTrainSubjects$Subject)
+
+#Load testing data
+
+fileName <- "./data/UCI HAR Dataset/test/subject_test.txt"
+rawTestSubjects <- read.table(fileName,col.names="Subject")
+
+fileName <- "./data/UCI HAR Dataset/test/y_test.txt"
+rawTestActivities  <- read.table(fileName,col.names = "ActivityClass")
+dtTestActivities <- data.table(rawTestActivities)
+rm(rawTestActivities)
+setkey(dtTestActivities,ActivityClass)
+
+dtActivityDetail <- merge(dtTestActivities,dtActivityLabels)
+
+
+fileName <- "./data/UCI HAR Dataset/test/x_test.txt"
+rawTestData <- read.table(fileName)
+
+tblTestData <- tbl_df(rawTestData)
+rm(rawTestData)
+
+tblTestSubSet  <- select(tblTestData,one_of(subColumnNames))
+#rm(tblTestData)
+colnames(tblTestSubSet) <- cleanSubNames
+tblTestSubSet <- mutate(tblTestSubSet, Activity = dtActivityDetail$Activity, Subject = rawTestSubjects$Subject)
+
+tblData <-  merge(tblTrainSubSet,tblTestSubSet,all = TRUE)
+
+#write.table(x = tblData,"./data/MergedData.txt", row.name=FALSE)
+save(tblData, file = "data/MergedData.RData")
 
 print("Data Creation Compplete")
