af1be7b3536744a9fab6686ed812b0b8e613a4d2
Joe Schaal <joe@schaal.net>
2014-11-17 09:56:02 -0500

    Completed README.md file and additional inline documentation for run_analysis.R

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-GetandCleanCourseProject
-========================
+Get and Clean Course Project
+============================
 
-Getting and Cleaning Data Course Project
+The purpose of this project is to to prepare tidy data set from a summary of raw data produced during measurement experiments of fitness oriented wearable computing devices produced by Samsung.
+
+The raw data contains numerous measurements of **three** dimensional acceleration from **six** different activities conducted on **30** experiment subjects in **two** conditions, *training* and *testing*.   The raw data consists of **68** measurements but the data we are interested in is only for those measurements which are **mean** and **standard deviation** calculations of specific dimensional accelerations.
+
+The resulting tidy data is produced by a script called **run_analysis.R**.   The script produces a number of out puts, stored in the data directory of the working directory, including an R formatted extract called **MergedData.RData** and the final **long formatted** tidy dataset in standard R text format called **TidyData.txt**.
+
+The script combines a number of different raw data sets that include separate information, such as the index of the test subject and the type of activity being measured. It  supplies appropriate field labels then produces a tidy data set in long format of the means of any included measurement of mean and standard orientation data.
+
+The script utilizes a number of R libraries and data types, including *data.frames*, *data.tables* and *dplyr oriented tables*.
+
+###The specific steps of the script are:
+
+1.  Read the common variable names (features)  of the raw measurement data into a data.frame
+2.  Read common activity names associated with the raw measurement data into a data.frame
+3.  Convert data.frames to data table to allow for easier merging of data with raw data
+4.  Read any number of raw Measurement files (in this case, 2, one for test data and one for training data)
+    +   Raw the raw raw subject file associated with raw data measurements
+    +   Read raw activity file associated with raw data measurements
+    +   Convert data.frames to data table to allow for easier merging of data with raw data
+    +   Merge activity labels with associated activities
+    +   Read raw measurement data
+    +   Convert data.frame to dplyr table for easier manipulation
+    +   identify and select only those columns that capture mean and standard deviations
+    +   Rename columns that do not reference specific x/y/z dimensions as other
+    +   Subset data to include only those measurements that pertain to mean and standard deviation measurements
+    +   Apply clean column names to table
+    +   Combine activity and subject details with raw data
+    +   Append processed data to consolidated processed data frame
+5. Save raw transformed data 
+6. Perform a series of dplyr orient transformations to convert untidy data to summarized, long formatted tidy data
+7.  Write final tidy data extract file
+
+ 
--- run_analysis.R
+++ run_analysis.R
@@ -9,80 +9,100 @@ activityFiles <- c("./data/UCI HAR Dataset/train/y_train.txt","./data/UCI HAR Da
 measurementFiles <- c("./data/UCI HAR Dataset/train/x_train.txt","./data/UCI HAR Dataset/test/x_test.txt")
 dfData <- data.frame()
 
-#Load Common Data
+#   Load Common Data
 print("Loading Common Data")
 
+#   Read common variable names (features)  of the raw measurement data
 fileName <- "./data/UCI HAR Dataset/features.txt"
 features <- read.table(fileName, col.names = c("field","feature"), stringsAsFactors = FALSE)
 fieldNames <- features[,2]
 rm(features)
 
+#   Read common activity names assocaited with the raw measurement data
 fileName <- "./data/UCI HAR Dataset/activity_labels.txt"
 rawActivityLabels <- read.table(fileName, col.names = c("ActivityClass","Activity"))
+
+#   Convert data.frame to data table to allow for easier merging of data with raw data
 dtActivityLabels <- data.table(rawActivityLabels)
 rm(rawActivityLabels)
 setkey(dtActivityLabels,ActivityClass)
 
-#Load Measurements
+#   Load Raw Measurement Data Files
 
 print("Loading Training Data")
 for (i in 1:length(dataSetNames)) {
     
-    
+    #   Read raw subject file associated with raw data measurements
     fileName <- subjectFiles[i]
     print(paste("Reading Subject File:",fileName))
     rawTrainSubjects <- read.table(fileName,col.names="Subject")
     
-    
+    #   Read raw activity file associated with raw data measurements
     fileName <- activityFiles[i]
     print(paste("Reading Subject Activity File:",fileName))
     rawTrainActivities  <- read.table(fileName,col.names = "ActivityClass")
+    
+    #   Convert data.frame to data table to allow for easier merging of data with raw data
     dtTrainActivities <- data.table(rawTrainActivities)
     rm(rawTrainActivities)
     setkey(dtTrainActivities,ActivityClass)
-    
+
+    #   Merge activity lables with assocaited actiivities
     dtActivityDetail <- merge(dtTrainActivities,dtActivityLabels)
     
-    
+    #   Read raw measurement data
     fileName <- measurementFiles[i]
     print(paste("Reading Subject Measurement File:",fileName))
     rawTrainData <- read.table(fileName)
     print(paste("Records Read:",as.character(nrow(rawTrainData))))
-    
+
+    #   Convert data.frame to dplyr table for easier manipulation
     print("Transforming Data")
     tblTrainData <- tbl_df(rawTrainData)
     rm(rawTrainData)
     
+    #   identify and select only those columns that capture mean and standard deviations
     columnPatterns<- "mean\\(\\)|std\\(\\)"
     subColumns  <- grep(columnPatterns,fieldNames)
     subColumnNames  <- colnames(tblTrainData)[subColumns]
     tblTrainSubSet  <- select(tblTrainData,one_of(subColumnNames))
     rm(tblTrainData)
-    
+
+    #   Rename columns that do not reference specifc x/y/z dimensions as other
     columnPatterns<- "\\(\\)"
     cleanNames <- gsub(columnPatterns,"",fieldNames)
     cleanSubNames <- cleanNames[subColumns]
     xyzDimensions <-  grep("X$|Y$|Z$",cleanSubNames)
     cleanSubNames[-xyzDimensions] <- paste(cleanSubNames[-xyzDimensions],"-Other",sep="")
-    
+
+    #   Apply clean column names to table
     colnames(tblTrainSubSet) <- cleanSubNames
+
+    #   Combine activity and subject details with raw data
     tblTrainSubSet <- mutate(tblTrainSubSet, Activity = dtActivityDetail$Activity, Subject = rawTrainSubjects$Subject)
     rm(rawTrainSubjects)
+
+    #   Append processed data to consolidated processed data frame
     dfData <- rbind(dfData,tblTrainSubSet)
     rm(tblTrainSubSet)
 
     
 }
 
+#   Save raw transformed data 
 print(paste("Total Data Transformed:",as.character(nrow(dfData))))
 save(dfData, file = "data/MergedData.RData")
 
 print("Data Merging Complete")
 
+#   Start tidy data routines  
 print("Tidying Data")
 
 tblData <- tbl_df(dfData)
 
+#   Start tidy data routines  
+#   Conver wide formatted data to long format
+#   group and summarize statistics
 tblData %>%
     gather(sensor_stat_dim,Measure,1:66,-(67:68)) %>%
     separate(sensor_stat_dim,c("Sensor","Statistic","Dimension")) %>%
@@ -91,6 +111,7 @@ tblData %>%
     summarize(mean(Measure)) %>%
     arrange(Subject,Activity,Sensor,Dimension,Statistic) -> tblTidy
 
+#   Write final tidy data extract file
 write.table(x = tblTidy,"./data/TidyData.txt", row.name=FALSE)
 
 print("Data Tidying Complete")
